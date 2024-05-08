524bfdfe19d81625e6ad04edeb6b7b84a9b7183d
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-05-08 17:53:05 +0300

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+""" Exporting csv files"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    """Read reddit API and return number subscribers """
+    headers = {'user-agent': '/u/ledbag123 API Python for alx-system-engineering'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return r.json()["data"]["subscribers"]
     else:
         return 0
